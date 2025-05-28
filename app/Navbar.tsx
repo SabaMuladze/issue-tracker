00912de1de157cffaacf6a8dbb464240f4467789
SabaMuladze <muladze66@bk.ru>
2025-05-28 21:59:42 +0400

    make active classes and issues page

--- app/Navbar.tsx
+++ app/Navbar.tsx
@@ -1,8 +1,12 @@
+"use client";
 import Link from "next/link";
 import React from "react";
 import { FaBug } from "react-icons/fa";
+import classNames from "classnames";
+import { usePathname } from "next/navigation";
 
 const Navbar = () => {
+  const currentPath = usePathname();
   const list = [
     { label: "Dashboard", href: "/" },
     { label: "Issues", href: "/issues" },
@@ -16,7 +20,11 @@ const Navbar = () => {
         {list.map((item) => (
           <li key={item.href}>
             <Link
-              className="text-zinc-500 hover:text-zinc-800"
+              className={classNames({
+                "text-zinc-900": currentPath == item.href,
+                "text-zinc-400": currentPath !== item.href,
+                "hover:text-zinc-800": true,
+              })}
               href={item.href}
             >
               {item.label}
